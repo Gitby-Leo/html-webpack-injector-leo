7db3e70870afc2225984b3d6b1ae6a07693fea36
leo.qin <747436776@qq.com>
2019-08-13 18:13:00 +0800

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+//Copyright by https://github.com/thearchitgarg/html-webpack-injector/
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+function getHeadAndBodyChunks(chunks) {
+    const headChunks = [];
+    const bodyChunks = [];
+
+    chunks.forEach(chunk => {
+        // add chunk.attributes.href
+        if ((chunk.attributes.src && chunk.attributes.src.includes("_head")) || chunk.attributes.href) {
+            headChunks.push(chunk);
+        } else {
+            bodyChunks.push(chunk);
+        }
+    });
+
+    return {
+        headChunks,
+        bodyChunks
+    };
+}
+
+class HtmlWebpackInjectorPlugin {
+    apply(compiler) {
+        // HtmlWebpackPlugin version 4.0.0-beta.5
+        if (HtmlWebpackPlugin.getHooks) {
+            compiler.hooks.compilation.tap('HtmlWebpackInjectorPlugin', (compilation) => {
+                HtmlWebpackPlugin.getHooks(compilation).alterAssetTagGroups.tapAsync(
+                    'HtmlWebpackInjectorPlugin', (data, callback) => {
+
+                        const ch = getHeadAndBodyChunks([...data.headTags, ...data.bodyTags]);
+
+                        data.headTags = ch.headChunks;
+                        data.bodyTags = ch.bodyChunks;
+
+                        callback(null, data)
+                    }
+                )
+            });
+        } else {
+            // HtmlWebpackPlugin version 3.2.0
+            compiler.plugin("compilation", compilation => {
+                compilation.plugin("html-webpack-plugin-alter-asset-tags", data => {
+                    const ch = getHeadAndBodyChunks([...data.head, ...data.body]);
+                    data.head = ch.headChunks;
+                    data.body = ch.bodyChunks;
+                });
+            });
+        }
+    }
+}
+
+module.exports = HtmlWebpackInjectorPlugin;
